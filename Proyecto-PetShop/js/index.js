23aaf98fec7c01c3f04c1c000d4b1c8e643d00e7
Diana_Sauval <dianasauval@hotmail.com>
2022-01-10 16:38:59 -0300

    actualizando

---
+++ Proyecto-PetShop/js/index.js
@@ -0,0 +1,33 @@
+window.addEventListener("load", function() {
+    const iconoMenu = this.document.getElementById("icono-menu");
+    const menu = this.document.querySelector(".menu");
+    const linkOpciones = this.document.querySelectorAll("a");
+    
+    for (let i = 0; i < linkOpciones.length; i++) {
+        linkOpciones[i].addEventListener("mouseover",function () {
+            linkOpciones[i].classList.add("seleccionado");
+            
+        });
+        linkOpciones[i].addEventListener("mouseout",function () {
+            linkOpciones[i].classList.remove("seleccionado");
+            
+        });
+        
+    }
+
+});
+
+$(document).ready(function(){    
+    $("body").hide().fadeIn(3000);
+    let orientacion = matchMedia("(orientation: portrait)");
+    if (orientacion.matches) {
+        $("#logo-final").after($("#contenedor"));      
+    };
+
+    $("#icono-menu").click(function(){
+        $(".menu").addClass("mostrar");
+        $(".menu").slideToggle(500);
+        
+    });          
+
+});
